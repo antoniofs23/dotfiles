e08c733f6a9dc8809d349c0e5784a8e73750fb19
Antonio Fernandez <antoniofs23@gmail.com>
2024-01-04 14:36:52 -0500

    added vim + tmux mapping integration

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -1,4 +1,24 @@
 local plugins = {
+   {
+  "alexghergh/nvim-tmux-navigation",
+  event = "VeryLazy",
+  config = function()
+    local nvim_tmux_nav = require("nvim-tmux-navigation")
+    nvim_tmux_nav.setup({
+      disable_when_zoomed = true,
+      -- defaults to false
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+        last_active = "<C-\\>",
+        next = "<C-Space>",
+      },
+    })
+  end,
+},
+
   {
     "rcarriga/nvim-dap-ui",
     event = "VeryLazy",
@@ -90,7 +110,7 @@ local plugins = {
         "mypy",
         "ruff",
         "pyright",
-
+        "tree-sitter-cli"
       },
     },
   },
